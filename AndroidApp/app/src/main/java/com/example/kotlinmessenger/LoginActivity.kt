35f1e9074cd1278d309a816fb0727adf448151f3
Eric Rasmusson Lindström <eric.rl@me.com>
2020-01-14 10:01:58 +0100

    Init

---
+++ AndroidApp/app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.example.kotlinmessenger
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            Log.d("__Login", "Attempt login with email/pw: $email / $password")
+
+        }
+
+        back_to_register_textview.setOnClickListener {
+                finish()
+        }
+    }
+}
