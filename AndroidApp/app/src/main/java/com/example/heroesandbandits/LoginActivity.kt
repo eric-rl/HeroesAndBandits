361328f4ab31b8935abb86caa1a4ade255be0464
Peter Sjögren <peter.sjogren@live.se>
2020-01-14 14:34:38 +0100

    extra

--- AndroidApp/app/src/main/java/com/example/heroesandbandits/LoginActivity.kt
+++ AndroidApp/app/src/main/java/com/example/heroesandbandits/LoginActivity.kt
@@ -1,2 +1,27 @@
 package com.example.heroesandbandits
 
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            Log.d("__LoginActivity", "Attempt login with email/pw: $email / $password")
+
+        }
+
+        back_to_register_textview.setOnClickListener {
+            finish()
+        }
+    }
+}
