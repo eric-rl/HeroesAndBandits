738cd459fe95b94a9b5c15489191ddd405324363
Peter Sjögren <peter.sjogren@live.se>
2020-01-14 15:38:06 +0100

    fix

--- AndroidApp/app/src/main/java/com/example/heroesandbandits/RegisterActivity.kt
+++ AndroidApp/app/src/main/java/com/example/heroesandbandits/RegisterActivity.kt
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
 
 
         register_button_register.setOnClickListener {
