c88080906c32e34fe9a323a7b455613b45e34ae9
Peter Sjögren <peter.sjogren@live.se>
2020-01-16 10:58:58 +0100

    style

--- AndroidApp/app/src/main/java/com/example/heroesandbandits/Utils/MarvelRetrofit.kt
+++ AndroidApp/app/src/main/java/com/example/heroesandbandits/Utils/MarvelRetrofit.kt
@@ -10,115 +10,64 @@ import java.security.NoSuchAlgorithmException
 
 object MarvelRetrofit {
 
-        private const val LOG = false
-
-        private const val PUBLIC_KEY = "ed3da575ea342e591fc219b429c3c111"
-
-        private const val PRIVATE_KEY = "9985cf0115724bc7aacb0cb7c41ba2a172b17111"
-
-        private const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+    private const val LOG = false
+    private const val PUBLIC_KEY = "ed3da575ea342e591fc219b429c3c111"
+    private const val PRIVATE_KEY = "9985cf0115724bc7aacb0cb7c41ba2a172b17111"
+    private const val BASE_URL = "https://gateway.marvel.com/v1/public/"
 
     fun String.md5(): String {
 
         val MD5 = "MD5"
-
         try { // Create MD5 Hash
-
             val digest = MessageDigest
-
                 .getInstance(MD5)
-
             digest.update(this.toByteArray())
-
             val messageDigest = digest.digest()
 
             // Create Hex String
-
             val hexString = StringBuilder()
-
             for (aMessageDigest in messageDigest) {
-
                 var h = Integer.toHexString(0xFF and aMessageDigest.toInt())
-
                 while (h.length < 2) h = "0$h"
-
                 hexString.append(h)
-
             }
-
             return hexString.toString()
-
         } catch (e: NoSuchAlgorithmException) {
-
             e.printStackTrace()
-
         }
-
         return ""
-
     }
 
-        val marvelService: MarvelService = Retrofit.Builder()
+    val marvelService: MarvelService = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .client(getOkHttpClient())
+        .build()
+        .create(MarvelService::class.java)
 
-            .baseUrl(BASE_URL)
+    private fun getOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level =
+            if (LOG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
 
-            .addConverterFactory(GsonConverterFactory.create())
-
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-
-            .client(getOkHttpClient())
-
-            .build()
-
-            .create(MarvelService::class.java)
-
-
-        private fun getOkHttpClient(): OkHttpClient {
-
-            val logging = HttpLoggingInterceptor()
-
-            logging.level = if (LOG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
-
-
-            val builder = OkHttpClient.Builder()
-
-                .addInterceptor{ chain ->
-
-                    val original = chain.request()
-
-                    val originalHttpUrl = original.url()
-
-                    val timestamp = System.currentTimeMillis().toString()
-
-                    val hash = (timestamp + PRIVATE_KEY + PUBLIC_KEY).md5()
-
-
-                    val url = originalHttpUrl.newBuilder()
-
-                        .addQueryParameter("apikey", PUBLIC_KEY)
-
-                        .addQueryParameter("ts",timestamp)
-
-                        .addQueryParameter("hash", hash)
-
-                        .build()
-
-
-                    val requestBuilder = original.newBuilder()
-
-                        .url(url)
-
-
-                    val request = requestBuilder.build()
-
-                    chain.proceed(request)
-
-                }
-
-                .addInterceptor(logging)
-
-            return builder.build()
-
-        }
-
+        val builder = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val original = chain.request()
+                val originalHttpUrl = original.url()
+                val timestamp = System.currentTimeMillis().toString()
+                val hash = (timestamp + PRIVATE_KEY + PUBLIC_KEY).md5()
+                val url = originalHttpUrl.newBuilder()
+                    .addQueryParameter("apikey", PUBLIC_KEY)
+                    .addQueryParameter("ts", timestamp)
+                    .addQueryParameter("hash", hash)
+                    .build()
+                val requestBuilder = original.newBuilder()
+                    .url(url)
+                val request = requestBuilder.build()
+                chain.proceed(request)
+            }
+            .addInterceptor(logging)
+        return builder.build()
     }
+}
--- AndroidApp/app/src/main/java/com/example/heroesandbandits/Utils/MarvelService.kt
+++ AndroidApp/app/src/main/java/com/example/heroesandbandits/Utils/MarvelService.kt
@@ -7,19 +7,12 @@ import retrofit2.http.Query
 interface MarvelService {
 
     @GET("characters?limit=1")
-
     fun getAllCharacters(
-
         @Query("nameStartsWith") nameStartsWith: String? = null,
-
         @Query("name") byExactName: String? = null,
-
         @Query("orderBy") orderBy: String? = null,
-
         @Query("limit") limit: Int? = null,
-
         @Query("offset") offset: Int? = null
-
     ): Single<CharacterDataWrapper>
 
 }
