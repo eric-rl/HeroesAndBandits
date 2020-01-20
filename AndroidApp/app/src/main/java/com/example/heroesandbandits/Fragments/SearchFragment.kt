2d3cd8c117a5d6c52666355c503caed521c1e0f7
Tootoo2 <antonstromkarl@gmail.com>
2020-01-20 20:41:24 +0100

    added a disposable to improve the android lifecycle

--- AndroidApp/app/src/main/java/com/example/heroesandbandits/Fragments/SearchFragment.kt
+++ AndroidApp/app/src/main/java/com/example/heroesandbandits/Fragments/SearchFragment.kt
@@ -6,20 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
-import android.widget.RadioGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.heroesandbandits.R
 import com.example.heroesandbandits.Utils.MarvelRetrofit
 import com.example.heroesandbandits.ViewModel.SharedViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.fragment_search.*
 import kotlinx.android.synthetic.main.fragment_search.view.*
 
 class SearchFragment : Fragment() {
 
     private lateinit var sharedViewModel: SharedViewModel
     private var searchForHeroes: Boolean = true
+    private val disposables = CompositeDisposable()
 
     override fun onCreateView(
 
@@ -46,46 +47,50 @@ class SearchFragment : Fragment() {
     }
 
     private fun displaySearchResult() {
-        Log.d("---", view!!.search_input.text.toString())
+
+        val toBeDisposed: Disposable
 
         if (searchForHeroes) {
 
-            MarvelRetrofit.marvelService.searchForCharacter(view!!.search_input.text.toString())
-                .subscribeOn(Schedulers.newThread())
-                .subscribe { result, err ->
-                    if (err?.message != null) {
-                        Log.d("___", "Something went wrong: ${err.message}")
-                        replaceFragment(SearchNoResultFragment.newInstance())
-                    } else {
-                        if (result.data.results.isEmpty()) {
+            toBeDisposed =
+                MarvelRetrofit.marvelService.searchForCharacter(view!!.search_input.text.toString())
+                    .subscribeOn(Schedulers.newThread())
+                    .subscribe { result, err ->
+                        if (err?.message != null) {
+                            Log.d("___", "Something went wrong: ${err.message}")
                             replaceFragment(SearchNoResultFragment.newInstance())
                         } else {
-                            Log.d("'''", "$result")
-                            sharedViewModel.searchResults.clear()
-                            sharedViewModel.searchResults.addAll(result.data.results)
-                            replaceFragment(SearchResultFragment.newInstance())
-                        }
+                            if (result.data.results.isEmpty()) {
+                                replaceFragment(SearchNoResultFragment.newInstance())
+                            } else {
+                                Log.d("'''", "$result")
+                                sharedViewModel.searchResults.clear()
+                                sharedViewModel.searchResults.addAll(result.data.results)
+                                replaceFragment(SearchResultFragment.newInstance())
+                            }
 
+                        }
                     }
-                }
         } else {
-            MarvelRetrofit.marvelService.searchForSeries(view!!.search_input.text.toString())
-                .subscribeOn(Schedulers.newThread()).subscribe { result, err ->
-                    if (err?.message != null) {
-                        Log.d("___", "Something went wrong: ${err.message}")
-                        replaceFragment(SearchNoResultFragment.newInstance())
-                    } else {
-                        Log.d("___", "${result.data.results.get(0).title}")
-                        if (result.data.results.isEmpty()) {
+            toBeDisposed =
+                MarvelRetrofit.marvelService.searchForSeries(view!!.search_input.text.toString())
+                    .subscribeOn(Schedulers.newThread()).subscribe { result, err ->
+                        if (err?.message != null) {
+                            Log.d("___", "Something went wrong: ${err.message}")
                             replaceFragment(SearchNoResultFragment.newInstance())
                         } else {
-                            Log.d("'''", "$result")
-                            replaceFragment(SearchResultFragment.newInstance())
-                        }
+                            Log.d("___", "${result.data}")
+                            if (result.data.results.isEmpty()) {
+                                replaceFragment(SearchNoResultFragment.newInstance())
+                            } else {
+                                Log.d("'''", "$result")
+                                replaceFragment(SearchResultFragment.newInstance())
+                            }
 
+                        }
                     }
-                }
         }
+        disposables.add(toBeDisposed)
     }
 
     private fun replaceFragment(fragment: Fragment) {
@@ -103,6 +108,11 @@ class SearchFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("__", "hej från search")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposables.dispose()
     }
 }
 
